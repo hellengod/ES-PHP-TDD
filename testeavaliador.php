2c5283c47e623e3341b646c7a8303ccbc6d6732f
Hellen Bianchini <hellenbiangod@gmail.com>
2025-05-01 16:42:51 -0300

    Implementa exemplo de teste automatizado utilizando Arrange Act Assert

--- testeavaliador.php
+++ testeavaliador.php
@@ -7,6 +7,7 @@ use Alura\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
+// Arrange - Given - Arrumo a casa para o teste
 $leilao = new Leilao('Fiat 147 0KM');
 
 $maria = new Usuario('maria');
@@ -16,8 +17,19 @@ $leilao->recebeLance(new Lance($joao, 2000));
 $leilao->recebeLance(new Lance($maria, 2500));
 
 $leiloeiro = new Avaliador();
+
+//Act - When - Executo o codigo a ser testado
 $leiloeiro->avalia($leilao);
 
 $maiorValor = $leiloeiro->getMaiorValor();
 
-echo $maiorValor;
+//Assert - Then - Verifico se a saida e a esperada
+$valorEsperado = 2500;
+
+if ($valorEsperado == $maiorValor) {
+    echo "TESTE OK";
+} else {
+    echo "TESTE FALHOU";
+}
+
+// echo $maiorValor;
