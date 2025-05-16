410a342842f5ceedec1e0aec3c8c934e4ea60e9b
Hellen Bianchini <hellenbiangod@gmail.com>
2025-05-16 14:03:15 -0300

    refactor array key last

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -34,7 +34,7 @@ class Leilao
 
     private function isUltimoUsuario(Lance $lance): bool
     {
-        $ultimoLance = $this->lances[count($this->lances) - 1];
+        $ultimoLance = $this->lances[array_key_last($this->lances)];
         return $lance->getUsuario() == $ultimoLance->getUsuario();
     }
 }
